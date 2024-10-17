edbbeb3851744fd58cbc5e0a53332d305c29a8df
Kiraogi <golokron3@yandex.ru>
2024-10-17 20:17:50 +0300

    Ver 0.12.5

--- database.py
+++ database.py
@@ -4,11 +4,11 @@ from psycopg2 import sql
 def get_connection():
     try:
         connection = psycopg2.connect(
-            dbname="your_db_name",
+            dbname="Nero_db",
             user="your_db_user",
             password="your_db_password",
             host="your_db_host",
-            port="your_db_port"
+            port="5433"
         )
         return connection
     except Exception as error:
